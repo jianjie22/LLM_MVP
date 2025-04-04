08f4a514f232806d1ea289dfe2a4902ab1ec17a3
jianjiezzz <119864021+jianjiezzz@users.noreply.github.com>
2025-04-04 11:43:50 +0800

    Add database connection setup with Beanie and MongoDB

---
+++ app/database.py
@@ -0,0 +1,17 @@
+# app/database.py
+
+from motor.motor_asyncio import AsyncIOMotorClient
+from beanie import init_beanie
+from app.models import Conversation  # We'll create this model soon
+import asyncio
+
+MONGO_URI = "mongodb://localhost:27017"
+DB_NAME = "llm_db"
+
+client = AsyncIOMotorClient(MONGO_URI)
+db = client[DB_NAME]
+
+
+# This should be called during FastAPI startup
+async def init_db():
+    await init_beanie(database=db, document_models=[Conversation])
